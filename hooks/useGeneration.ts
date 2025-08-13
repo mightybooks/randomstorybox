0fe9df2991fda7733564f32784a8897c2fb5e7c7
mightybooks <novelstudylab@naver.com>
2025-08-13 23:49:41 +0900

    Update useGeneration.ts

--- hooks/useGeneration.ts
+++ hooks/useGeneration.ts
@@ -1,6 +1,6 @@
 import { useCallback, useRef, useState } from "react";
-import { GenerateRequest, GenerateResponse } from "../../lib/schemas";
-import { apiFetch } from "../../lib/apiClient";
+import { GenerateRequest, GenerateResponse } from "../lib/schemas";
+import { apiFetch } from "../lib/apiClient";
 
 export type GenPhase = "idle" | "writing" | "done";
 
